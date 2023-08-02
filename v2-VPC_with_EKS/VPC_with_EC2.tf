9f41b20749226d0b50fa171c9a775cbb28fc7e54
gredd182 <134130676+gredd182@users.noreply.github.com>
2023-08-02 18:19:25 +0530

    Update VPC_with_EC2.tf

--- v2-VPC_with_EKS/VPC_with_EC2.tf
+++ v2-VPC_with_EKS/VPC_with_EC2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "demo-server" {
  key_name = var.key 
  instance_type  = var.instance-type
  associate_public_ip_address = true
-subnet_id = aws_subnet.demo_subnet.id
+subnet_id = aws_subnet.demo_subnet-1.id
 vpc_security_group_ids = [aws_security_group.demo-vpc-sg.id]
 }
 
@@ -17,16 +17,28 @@ resource "aws_vpc" "demo-vpc" {
 }
 
 // Create Subnet
-resource "aws_subnet" "demo_subnet" {
+resource "aws_subnet" "demo_subnet-1" {
   vpc_id     = aws_vpc.demo-vpc.id 
   cidr_block = var.subnet1-cidr
   availability_zone = var.subent_az
+  map_public_ip_on_launch = "true"
 
   tags = {
-    Name = "demo_subnet"
+    Name = "demo_subnet-1"
   }
 }
 
+// Create Subnet
+resource "aws_subnet" "demo_subnet-2" {
+  vpc_id     = aws_vpc.demo-vpc.id 
+  cidr_block = var.subnet2-cidr
+  availability_zone = var.subent-2_az
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "demo_subnet-2"
+  }
+}
 // Create Internet Gateway
 
 resource "aws_internet_gateway" "demo-igw" {
@@ -50,8 +62,14 @@ resource "aws_route_table" "demo-rt" {
 }
 
 // associate subnet with route table 
-resource "aws_route_table_association" "demo-rt_association" {
-  subnet_id      = aws_subnet.demo_subnet.id 
+resource "aws_route_table_association" "demo-rt_association-1" {
+  subnet_id      = aws_subnet.demo_subnet-1.id 
+
+  route_table_id = aws_route_table.demo-rt.id
+}
+// associate subnet with route table 
+resource "aws_route_table_association" "demo-rt_association-2" {
+  subnet_id      = aws_subnet.demo_subnet-2.id 
 
   route_table_id = aws_route_table.demo-rt.id
 }
@@ -84,4 +102,14 @@ resource "aws_security_group" "demo-vpc-sg" {
   }
 }
 
+module "sgs" {
+  source = "./sg_eks"
+  vpc_id = aws_vpc.demo-vpc.id
+}
 
+module "eks" {
+  source = "./eks"
+  sg_ids = module.sgs.security_group_public
+  vpc_id = aws_vpc.demo-vpc.id
+  subnet_ids = [aws_subnet.demo_subnet-1.id,aws_subnet.demo_subnet-2.id]
+}
